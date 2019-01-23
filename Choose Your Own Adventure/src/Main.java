9646b8ed46370c4cd56c620efff8e6b318b5d697
milkbgm1 <milkbrand.com.ca.net.gov@gmail.com>
2019-01-22 21:37:57 -0800

    CYOA2
    CYOA  2

--- Choose Your Own Adventure/src/Main.java
+++ Choose Your Own Adventure/src/Main.java
@@ -4,48 +4,69 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+
+
     public static class Page{
         private int id;
         private int pathsAvailable;
         private boolean visted;
-        private int[] paths;
+        private int[] paths; //ints holding ids of pages in array pages
         Page(int id, int pathsAvailable, boolean visted) {
             this.id = id;
             this.pathsAvailable = pathsAvailable;
             this.visted = visted;
-        }
+            this.paths = new int[pathsAvailable];
+            if (pathsAvailable > 0) {
+                for (int i = 0; i < pathsAvailable; i++) {
+                    this.paths[i] = pagesData[id - 1][i+1];
 
-        public void setupPaths(){
-            for (int i = 0; i < pathsAvailable; i++) {
-                this.paths[i] = pagesData[id-1][i];
+                }
             }
         }
-
         public String toString() {
             return ("id:" + id + " pathsAvailable:" + pathsAvailable+ " visted:" + visted);
         }
     }
 
+    public static int visits = 0;
+    public static int lowest = 999999999;
     private static int[][] pagesData;
     private static ArrayList<Page> pages = new ArrayList<>();
 
-    public static void walk(int page){
-        if (pagesData[page][0] == 0){
+    public static void visit(Page page, int steps){
+        if (page.pathsAvailable==0){
+            page.visted = true;
+            visits++;
+            if (steps < lowest){
+                lowest = steps;
+            }
+        } else if (page.visted == false) {
+            page.visted = true;
+            visits++;
+            for (int i = 0; i < page.pathsAvailable; i++) {
+                visit(pages.get(page.paths[i]-1), steps+1);
+
+            }
 
         }
+
     }
 
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner in = new Scanner(new File("H:\\My Documents\\Steven-D\\Choose Your Own Adventure\\src\\in.txt"));
+        Scanner in = new Scanner(System.in);
         int N = Integer.parseInt(in.nextLine());
         pagesData = new int[N][N];
         for (int i = 0; i < N; i++){
             String temp[] = in.nextLine().split(" ");
-            pages.add(new Page(i+1, Integer.parseInt(temp[0]), false ));
-            for (int ii = 1; ii < pagesData[i][0]+1; ii++) {
-                pagesData[i][ii] = Integer.parseInt(temp[ii-1]);
+            for (int ii = 1; ii < Integer.parseInt(temp[0])+1; ii++) {
+                pagesData[i][ii] = Integer.parseInt(temp[ii]);
             }
+            pages.add(new Page(i+1, Integer.parseInt(temp[0]), false ));
+
+
         }
+
+
         /*
         for (int i = 0; i < N; i++){
             System.out.println(pagesData[i][0]);
@@ -57,8 +78,20 @@ public class Main {
 
         */
 
-        for (int i = 0; i < pages.size(); i++) {
-            System.out.println(pages.get(i));
+
+
+        visit(pages.get(0), 1);
+
+
+        if (visits == pages.size()){
+            System.out.println("Y");
+        } else {
+            System.out.println("N");
         }
+
+        System.out.println(lowest);
+
+
     }
+
 }
